1220bdf98f87a1d5a4c9c2b493c6c1788d21e76a
wahab-weeinvent <itinteractiveacademy@gmail.com>
2025-02-01 13:05:28 +0500

    secrets

--- obj/Debug/net9.0/testblazor.AssemblyInfo.cs
+++ obj/Debug/net9.0/testblazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testblazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0237ba8a150f827c17353be4b85bc6ac7ce5473a")]
 [assembly: System.Reflection.AssemblyProductAttribute("testblazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testblazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
