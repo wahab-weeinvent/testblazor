6f4525d701917b5eed30f824517baac8858d6b64
wahab-weeinvent <itinteractiveacademy@gmail.com>
2025-02-01 14:41:15 +0500

    github b

--- obj/Release/net8.0/linux-x64/testblazor.AssemblyInfo.cs
+++ obj/Release/net8.0/linux-x64/testblazor.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("testblazor")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efff5cf0a7b8d18827f3a6eb720315ced98bda09")]
 [assembly: System.Reflection.AssemblyProductAttribute("testblazor")]
