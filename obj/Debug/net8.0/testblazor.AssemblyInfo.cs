6875e3d58065d96b0c33d35e8c2c318b8e48bd88
wahab-weeinvent <itinteractiveacademy@gmail.com>
2025-02-02 11:03:06 +0500

    github 15

--- obj/Debug/net8.0/testblazor.AssemblyInfo.cs
+++ obj/Debug/net8.0/testblazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testblazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c897d1f09c2d650af9e91e6f8418f3335d53ac5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa0328a3eb3e93287e0d87292c6358d0a2def2a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("testblazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testblazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
